e70700404a9c31c01b836cb5243f8755c043dfe1
Farkhan Azmi <fliazmi@gmail.com>
2021-07-18 23:05:38 +0700

    change icon, change messy code, change photo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import { DashboardScreen } from "./ui/screens";
-// import { Nav, NavItem } from "./ui/components";
+import { Nav, NavItem } from "./ui/components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      {/* <Nav>
-        <NavItem href="/" isActive>
-          Dashboard
-        </NavItem>
-        <NavItem href="/">Cooming Soon</NavItem>
-        <NavItem href="/">Cooming Soon</NavItem>
-      </Nav> */}
       <Switch>
         <Route path="/">
           <DashboardScreen />
--- src/ui/screens/dashboard/index.js
+++ src/ui/screens/dashboard/index.js
@@ -1,43 +1,33 @@
-import photo from "../../../assets/images/photo.png";
+import photo from "../../../assets/images/me.jpg";
+
 export const DashboardScreen = () => {
+  console.log("sdasd");
   return (
-    <section className="relative bg-khan h-screen">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <section className="hero container max-w-screen-lg mx-auto pb-4">
-              <img
-                alt=""
-                src={photo}
-                width="150"
-                height="150"
-                className="mx-auto"
-              />
-            </section>
-            <a
-              href="https://www.youtube.com/watch?v=5XD_f-OaX-o&ab_channel=DevNobody"
-              className="text-5xl md:text-6xl font-semibold  leading-tighter tracking-tighter mb-4 text-gray-300"
-              data-aos="zoom-y-out"
-            >
-              Farkhan{" "}
-              <span className="bg-clip-text text-transparent font-extrabold bg-gradient-to-r from-red-500 to-yellow-500">
-                Azmi
-              </span>
-            </a>
-            <div className="max-w-3xl mx-auto">
-              <p
-                className="text-xl font-thin text-gray-100"
-                data-aos="zoom-y-out"
-                data-aos-delay="150"
-              >
-                Ordinary Khan
-              </p>
-            </div>
-          </div>
+    <section className="flex bg-khan h-screen w-screen bg-center">
+      <a
+        href="https://www.linkedin.com/in/farkhanazmi/"
+        className="flex mx-auto my-auto flex-col items-center md:flex-row"
+      >
+        <img
+          alt="Ordinary Guy"
+          src={photo}
+          className="rounded-full w-36 h-36"
+        />
+        <div className="md:ml-3 text-center md:text-left">
+          <p
+            className="flex text-5xl md:text-6xl font-semibold  text-gray-300 flex-row items-center"
+            data-aos="zoom-y-out"
+          >
+            Farkhan
+            <span className="bg-clip-text text-transparent font-black bg-gradient-to-r from-red-600 to-yellow-600 ml-2">
+              Azmi
+            </span>
+          </p>
+          <p className="text-xl md:text-2xl font-thin text-gray-100 ">
+            Ordinary Khan
+          </p>
         </div>
-      </div>
+      </a>
     </section>
   );
 };
